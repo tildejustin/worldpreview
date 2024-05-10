3b774868843f8b350be545ba709447f9a8d7e8a4
tildejustin <spacepepper3.14@gmail.com>
2024-05-10 18:28:51 -0400

    fix crash due to spawner attempting to render a mob

---
+++ src/main/java/me/voidxwalker/worldpreview/mixin/client/render/block/MobSpawnerBlockEntityRendererMixin.java
@@ -0,0 +1,21 @@
+package me.voidxwalker.worldpreview.mixin.client.render.block;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.*;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.LevelLoadingScreen;
+import net.minecraft.client.render.block.entity.MobSpawnerBlockEntityRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.*;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(MobSpawnerBlockEntityRenderer.class)
+public abstract class MobSpawnerBlockEntityRendererMixin {
+    @WrapOperation(
+            method = "render(Lnet/minecraft/block/entity/MobSpawnerBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/MobSpawnerLogic;getRenderedEntity(Lnet/minecraft/world/World;)Lnet/minecraft/entity/Entity;")
+    )
+    private Entity cancelEntityRenderOnPreview(MobSpawnerLogic instance, World world, Operation<Entity> original) {
+        return MinecraftClient.getInstance().currentScreen instanceof LevelLoadingScreen ? null : original.call(instance, world);
+    }
+}
